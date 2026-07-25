dae9f94f505b7ec6ddd4d67ed241ae90d0c78674
Krish Mistry <mistrykrish2005@gmail.com>
2026-07-25 20:23:18 +0530

    Time: 0 ms (100%), Space: 12.3 MB (53.86%) - LeetHub - Time: O(n), Space: O(1)

--- 0066-plus-one/0066-plus-one.py
+++ 0066-plus-one/0066-plus-one.py
@@ -1,21 +1,14 @@
 class Solution(object):
-
-  def plusOne(self, digits):
-    n = len(digits)
-    high = n - 1
-
-    if digits[high] == 9:
-
-      for i in range(high, -1, -1):
-        if digits[i] < 9:
-          digits[i] += 1
-          return digits
-        digits[i] = 0
-
-      return [1] + digits
-
-    digits[high] += 1
-    return digits
+    def plusOne(self, digits):
+        n = len(digits)
+        for i in range(n - 1, -1, -1):
+            if digits[i] < 9:
+                digits[i] += 1
+                return digits
+            digits[i] = 0
+        digits.append(0)     
+        digits[0] = 1
+        return digits
 
 # Synced seamlessly with LeetHub Pro
 # Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
