89bfb96095af7b4c590766c4a77cc380d51bd5e0
Krish Mistry <mistrykrish2005@gmail.com>
2026-07-28 17:58:11 +0530

    Time: 0 ms (100%), Space: 12.3 MB (91.22%) - LeetHub - Time: O(n), Space: O(1)

---
+++ 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def middleNode(self, head):
+        slow = head
+        fast = head 
+
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+
+        return slow
+        
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
