aa9f5e46b3f6115887597f2a49c74f67036fcd3a
Krish Mistry <mistrykrish2005@gmail.com>
2026-08-28 22:14:26 +0530

    Time: 0 ms (100%), Space: 12.9 MB (66.24%) - LeetHub - Time: O(log n), Space: O(1)

---
+++ 0035-search-insert-position/0035-search-insert-position.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def searchInsert(self, nums, target):
+        low = 0
+        high = len(nums) - 1
+
+        while low <= high:
+            mid = (low + high) // 2
+
+            if nums[mid] == target:
+                return mid
+
+            elif nums[mid] < target:
+                low = mid + 1
+
+            else:
+                high = mid - 1
+
+        return low
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
