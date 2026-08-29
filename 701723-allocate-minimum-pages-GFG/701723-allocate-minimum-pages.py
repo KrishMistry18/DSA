3e7294b957ef133085db97c0490767735cc197ae
Krish Mistry <mistrykrish2005@gmail.com>
2026-08-29 19:41:10 +0530

    Added solution - Time: O(n * log(sum(arr))), Space: O(1)

---
+++ 701723-allocate-minimum-pages-GFG/701723-allocate-minimum-pages.py
@@ -0,0 +1,42 @@
+class Solution:
+    def findPages(self, arr, k):
+        n = len(arr)
+
+    # More students than books
+        if k > n:
+            return -1
+
+        low = max(arr)
+        high = sum(arr)
+        ans = high
+
+        def canAllocate(limit):
+            students = 1
+            pages = 0
+
+            for book in arr:
+                if pages + book <= limit:
+                    pages += book
+                else:
+                    students += 1
+                    pages = book
+
+                    if students > k:
+                        return False
+
+            return True
+
+        while low <= high:
+            mid = (low + high) // 2
+
+            if canAllocate(mid):
+                ans = mid
+                high = mid - 1
+            else:
+                low = mid + 1
+
+        return ans
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
