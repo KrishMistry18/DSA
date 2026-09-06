083812e45d91aa2c129bf71f3f07e1dab3f7af50
Krish Mistry <mistrykrish2005@gmail.com>
2026-09-06 20:25:11 +0530

    Added solution - Time: O(n log k), Space: O(k)

---
+++ 701747-kth-smallest-GFG/701747-kth-smallest.py
@@ -0,0 +1,19 @@
+import heapq
+
+class Solution:
+    def kthSmallest(self, arr, k):
+        heap = []
+
+        for num in arr:
+            heapq.heappush(heap, -num)
+
+            if len(heap) > k:
+                heapq.heappop(heap)
+
+        return -heap[0]
+
+
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
