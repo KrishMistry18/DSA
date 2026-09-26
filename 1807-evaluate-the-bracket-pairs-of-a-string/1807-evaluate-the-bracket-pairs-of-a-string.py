fa749a7fab7d0f5f473d4ef0078a05ff15fd2ca4
Krish Mistry <mistrykrish2005@gmail.com>
2026-09-26 20:19:28 +0530

    Time: 74 ms (64.29%), Space: 55.7 MB (82.14%) - LeetHub - Time: O(N + K), Space: O(N + K)

---
+++ 1807-evaluate-the-bracket-pairs-of-a-string/1807-evaluate-the-bracket-pairs-of-a-string.py
@@ -0,0 +1,35 @@
+class Solution(object):
+    def evaluate(self, s, knowledge):
+        mp = {}
+
+        for key, value in knowledge:
+            mp[key] = value
+
+        result = []
+        i = 0
+
+        while i < len(s):
+            if s[i] == '(':
+                j = i + 1
+
+                while s[j] != ')':
+                    j += 1
+
+                key = s[i + 1:j]
+
+                if key in mp:
+                    result.append(mp[key])
+                else:
+                    result.append('?')
+
+                i = j + 1
+
+            else:
+                result.append(s[i])
+                i += 1
+
+        return ''.join(result)
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
